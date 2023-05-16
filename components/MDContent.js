8cabfb65b88b99286a770ccf1e477a8748666e6a
kirass79 <126791009+kirass79@users.noreply.github.com>
2023-05-16 12:46:24 +0900

    Update MDContent.js

--- components/MDContent.js
+++ components/MDContent.js
@@ -43,8 +43,8 @@ function MDContent({content, backLinks, handleOpenNewContent}) {
         <div className="markdown-rendered">
             <Alert severity="info">
                 <AlertTitle>Want to know more?</AlertTitle>
-                🌱 <strong>Follow</strong> or <strong>DM</strong> me on Twitter at <span><a
-                href="https://twitter.com/tuancm">@tuancm</a></span>
+                🌱 <strong>Follow</strong> or <strong>DM</strong> me on Pinterest at <span><a
+                href="https://www.pinterest.co.kr/lifesproutz/">@BrotherJOE</a></span>
             </Alert>
             <div dangerouslySetInnerHTML={{__html: content}}/>
             {/*<button onClick={handleInternalLinkClick}>Click me</button>*/}
